80b645a300fec0a9cccf6e8c9e5d08ef72582d0d
AleGon98 <ag435@lindenwood.edu>
2020-01-07 19:44:48 -0600

    Initial commit

---
+++ src/api/dataService.js
@@ -0,0 +1,150 @@
+export default function() {
+    return Promise.resolve(
+      [
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 120,
+            transactionDt: "10-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 88,
+            transactionDt: "10-21-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 66,
+            transactionDt: "10-21-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 53,
+            transactionDt: "11-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 77,
+            transactionDt: "11-21-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 147,
+            transactionDt: "12-01-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 87,
+            transactionDt: "12-04-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 5,
+            transactionDt: "12-03-2019"
+          },
+          {
+            custid: 1,
+            name: "Venezuela",
+            amt: 224,
+            transactionDt: "12-21-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 111,
+            transactionDt: "10-01-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 69,
+            transactionDt: "10-21-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 4,
+            transactionDt: "11-01-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 187,
+            transactionDt: "11-21-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 222,
+            transactionDt: "12-01-2019"
+          },
+          {
+            custid: 2,
+            name: "Italy",
+            amt: 99,
+            transactionDt: "12-21-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 140,
+            transactionDt: "10-01-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 54,
+            transactionDt: "10-21-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 33,
+            transactionDt: "11-01-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 189,
+            transactionDt: "11-21-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 250,
+            transactionDt: "12-01-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 20,
+            transactionDt: "12-21-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 10,
+            transactionDt: "01-01-2019"
+          },
+          {
+            custid: 3,
+            name: "Spain",
+            amt: 81,
+            transactionDt: "01-04-2019"
+          },
+          {
+            custid: 4,
+            name: "USA",
+            amt: 120,
+            transactionDt: "06-21-2019"
+          }
+      ]
+    );
+  };
